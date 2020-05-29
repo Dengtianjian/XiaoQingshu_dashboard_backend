d17af74d529b89ed92ea63284c656e04c2134471
DengTianjian <mail@isdtj.com>
2020-05-29 12:37:11 +0800

    完善API入口
    增加验证接口模块是否存在
    响应请求数据

--- index.php
+++ index.php
@@ -2,16 +2,33 @@
 
 require_once("./core.php");
 
-$Modules = [];
+header("Access-Control-Allow-Origin:*");
+
+$Modules = [
+  "user"
+];
 
 $module = addslashes($_GET['module']);
 
-$password = "123456";
-$passPeppered = hash_hmac("sha256", $password, $USERPASS['slat']);
-$passHashed = password_hash($passPeppered, PASSWORD_BCRYPT);
-$hash = '$2y$10$KnKlctFi3PK6ImHaIqbdxOrXc4AMWTfFTPhckWrPZhu7E4Hm5G/FK';
-if (password_verify($passPeppered, $hash)) {
-  echo "matches";
-} else {
-  echo "no";
+if (!in_array($module, $Modules)) {
+  HTTP::error("MODULES_DOES_NOT_EXISTS");
 }
+
+include_once("./module/{$module}.php");
+$moduleInstance = new $module();
+$requestMethod = addslashes($_GET['method']);
+if (!in_array($requestMethod, $moduleInstance->methods)) {
+  HTTP::error("MODULES_METHOD_DOES_NOT_EXISTS");
+}
+unset($_GET['method'], $_GET['module']);
+$responseResult = $moduleInstance->{$requestMethod}();
+Response(null, $responseResult);
+// $password = "123456";
+// $passPeppered = hash_hmac("sha256", $password, $USERPASS['slat']);
+// $passHashed = password_hash($passPeppered, PASSWORD_BCRYPT);
+// $hash = '$2y$10$KnKlctFi3PK6ImHaIqbdxOrXc4AMWTfFTPhckWrPZhu7E4Hm5G/FK';
+// if (password_verify($passPeppered, $hash)) {
+//   echo "matches";
+// } else {
+//   echo "no";
+// }
