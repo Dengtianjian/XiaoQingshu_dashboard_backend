4b0b817f1f8dcb10c44dd2c9c9eee1541853e5cd
DengTianjian <mail@isdtj.com>
2020-06-03 10:32:21 +0800

    table类增加方法
    增加插入记录方法
    增加删除记录方法

--- class/table.class.php
+++ class/table.class.php
@@ -6,14 +6,28 @@ if (!defined("IN_C")) {
 
 class Table
 {
-
-  private $pk = "";
-  private $name = "";
+  function insert($data, $returnId = true)
+  {
+    $insertKey = implode(",", array_keys($data));
+    $insertValue = DB::charSpilt(",", array_values($data));
+    $result = DB::query("INSERT INTO `&t`($insertKey) VALUES($insertValue) ", [
+      $this->name
+    ]);
+    if ($returnId) {
+      return DB::lastInsertId();
+    }
+    return $result;
+  }
 
   function fetch_all()
   {
     return DB::fetch("SELECT * FROM `%t`", [
       $this->name
     ]);
+  }
+
+  function delete($conditionSql = "", $params = [])
+  {
+    return DB::delete($this->name, $conditionSql, $params);
   }
 }
