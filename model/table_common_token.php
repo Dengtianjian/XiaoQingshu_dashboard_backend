256d6c68219fa70d254946742c39decbb42dd6e0
DengTianjian <mail@isdtj.com>
2020-06-03 10:33:48 +0800

    token模型优化

--- model/table_common_token.php
+++ model/table_common_token.php
@@ -8,7 +8,18 @@ include_once includeClass("table"); //数据表类
 
 class Table_common_token extends Table
 {
-  function getToken()
+  public $name = "common_token";
+  function cleanExpireToken()
   {
+    return $this->delete("WHERE `token_expire`<&n", [
+      time()
+    ]);
+  }
+  function fetch_by_token($token)
+  {
+    return DB::fetch_first("SELECT * FROM `&t` WHERE `token_content`=&s", [
+      $this->name,
+      $token
+    ]);
   }
 }
