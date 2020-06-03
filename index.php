659421a3cf702c878820af8e806a5b275bca55b6
DengTianjian <mail@isdtj.com>
2020-06-03 10:34:40 +0800

    优化 入口文件

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+header("Content-type: application/json;charset=utf-8");
+header('Content-language: cn');
+
 require_once("./core.php");
 
-header("Access-Control-Allow-Origin:*");
+$_REQUEST = $_POST = $_GET = array_merge($_GET, $_POST, $_REQUEST);
 
 $Modules = [
   "user"
@@ -11,24 +17,33 @@ $Modules = [
 $module = addslashes($_GET['module']);
 
 if (!in_array($module, $Modules)) {
-  HTTP::error("MODULES_DOES_NOT_EXISTS");
+  HTTP::error(null, "MODULES_DOES_NOT_EXISTS");
 }
 
 include_once("./module/{$module}.php");
 $moduleInstance = new $module();
 $requestMethod = addslashes($_GET['method']);
 if (!in_array($requestMethod, $moduleInstance->methods)) {
-  HTTP::error("MODULES_METHOD_DOES_NOT_EXISTS");
+  HTTP::error(null, "MODULES_METHOD_DOES_NOT_EXISTS");
 }
+
+//验证token
+if (isset($_GET['token'])) {
+  $token = addslashes($_GET['token']);
+  include_once includeClass("token");
+  $Token = new Token();
+  $result = $Token->checkExpire($token);
+  if (isset($result['code'])) {
+    switch ($result['code']) {
+      case 0:
+        HTTP::error("凭证不存在", 401, 4010001);
+        break;
+      default:
+        HTTP::error("凭证已过期", 401, 4010002);
+    }
+  }
+}
+
 unset($_GET['method'], $_GET['module']);
 $responseResult = $moduleInstance->{$requestMethod}();
-Response(null, $responseResult);
-// $password = "123456";
-// $passPeppered = hash_hmac("sha256", $password, $USERPASS['slat']);
-// $passHashed = password_hash($passPeppered, PASSWORD_BCRYPT);
-// $hash = '$2y$10$KnKlctFi3PK6ImHaIqbdxOrXc4AMWTfFTPhckWrPZhu7E4Hm5G/FK';
-// if (password_verify($passPeppered, $hash)) {
-//   echo "matches";
-// } else {
-//   echo "no";
-// }
+Response($responseResult);
