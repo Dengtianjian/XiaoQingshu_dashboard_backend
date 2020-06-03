4f1f4214ab1f9a64b477b3378346b6652fcbc31f
DengTianjian <mail@isdtj.com>
2020-06-03 13:57:41 +0800

    增加注释

--- index.php
+++ index.php
@@ -8,27 +8,41 @@ header('Content-language: cn');
 
 require_once("./core.php");
 
+//把 $_GET $_POST $_REQUEST 合并在一起
 $_REQUEST = $_POST = $_GET = array_merge($_GET, $_POST, $_REQUEST);
 
+//API 模块
 $Modules = [
   "user"
 ];
 
-$module = addslashes($_GET['module']);
+//需要 验证凭证的方法
+$verifyMethods = [
+  "verify"
+];
 
+$module = addslashes($_GET['module']); //获取请求的模块名称
+//判断是否存在 请求的模块
 if (!in_array($module, $Modules)) {
   HTTP::error(null, "MODULES_DOES_NOT_EXISTS");
 }
 
+//加载 请求的模块文件
 include_once("./module/{$module}.php");
+//实例化 请求的模块文件
 $moduleInstance = new $module();
+//获取 请求的模块所执行的方法
 $requestMethod = addslashes($_GET['method']);
+//判断 所执行的方法是否存在请求的模块里
 if (!in_array($requestMethod, $moduleInstance->methods)) {
   HTTP::error(null, "MODULES_METHOD_DOES_NOT_EXISTS");
 }
 
 //验证token
-if (isset($_GET['token'])) {
+if (isset($_GET['token']) || in_array($requestMethod, $verifyMethods)) {
+  if (!isset($_GET['token']) && in_array($requestMethod, $verifyMethods)) {
+    HTTP::error([], "USER_IS_NOT_LOGGED_IN");
+  }
   $token = addslashes($_GET['token']);
   include_once includeClass("token");
   $Token = new Token();
