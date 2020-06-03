961a1da6e0b05d7dc1ccc9dbce5312aac6d72620
DengTianjian <mail@isdtj.com>
2020-06-03 10:34:24 +0800

    user模型优化

--- model/table_user.php
+++ model/table_user.php
@@ -8,4 +8,12 @@ include_once includeClass("table"); //数据表类
 
 class Table_user extends Table
 {
+  private $pk = "user_id";
+  private $name = "user";
+  function fetch_first_by_username($username)
+  {
+    return DB::fetch_first("SELECT * FROM `&t` WHERE `user_name`=&s", [
+      $this->name, "admin"
+    ]);
+  }
 }
